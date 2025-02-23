98cb9858de6be287daff6bbedc22e809da85a381
Enamul-Haque-Shojib <enamulhaqueshojib@gmail.com>
2025-02-24 01:11:53 +0600

    auth update

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,4 @@
 
-// import express, { Application } from "express";
-// import cors from "cors";
-
-
-// import { Server } from "socket.io";
-// import http from "http";
-// import globalErrorHandler from "./app/middlewares/globalErrorhandler";
-// import notFound from "./app/middlewares/notFound";
-// import router from "./app/routes";
-
-// const app: Application = express();
-
-//  const server = http.createServer(app);
-//     const io = new Server(server, {
-//       cors: { origin: ["http://localhost:5173"] },
-//     });
-
-//   app.use(
-//       cors({
-//         origin: ['http://localhost:5173'],
-//         credentials: true,
-//       }),
-//   );
-// app.use(express.json());
-
-// // Task routes
-// // app.use("/tasks", taskRoutes);
-// app.use('/api/', router);
-
-// app.use(globalErrorHandler);
-// app.use(notFound);
-
-// export {server, io};
 
 
 
